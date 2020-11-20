473eac6dc665d947adfbdd437da23975913f4369
AUGUSTO <acesarp@users.noreply.github.com>
2020-11-20 11:20:22 -0800

    augusto branch created

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
 let express = require('express');
 let app = express();
+let port = process.env.PORT || '5001';
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${app.get('port')}`);
+});
