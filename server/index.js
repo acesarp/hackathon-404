921807c1fa3b632037825eddd34b4ecb4baec60d
AUGUSTO <acesarp@users.noreply.github.com>
2020-11-20 12:01:45 -0800

    Edit json file, expanded get request

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
 let express = require('express');
 let app = express();
+let cors = require('cors');
+let path = require('path');
 let port = process.env.PORT || '5001';
 let router = express.Router();
 const moods = require('./data/moods.json');
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // 
 router.get('/:id', (req, res) => {
     console.info(req.params);
-    req.params.id;
-
-
-    res.send();
+    
+    const payload = moods.filter(value => value.id === req.params.id)
+    res.send(payload);
 });
-
-
 
 
 const server = app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${app.get('port')}`);
 });
+
+    module.exports = app;
