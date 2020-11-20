cd3e0cce610f2f58d14362bbe808f0795f2157ea
AUGUSTO <acesarp@users.noreply.github.com>
2020-11-20 11:25:29 -0800

    add first end point

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 let express = require('express');
 let app = express();
 let port = process.env.PORT || '5001';
+let router = express.Router();
+
+
+// 
+router.get('/', (req, res) => {
+
+    
+
+    res.send();
+});
+
+
+
 
 const server = app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${app.get('port')}`);
